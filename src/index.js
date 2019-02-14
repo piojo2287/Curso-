a39320b17472e3780283b7842e85ec1a7d27742a
piojo2287 <33067911+piojo2287@users.noreply.github.com>
2019-02-14 19:22:27 -0300

    correccion de envio de port

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const path = require("path");
 
 //setting
-app.set("port", 4000);
+app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.engine("html",require("ejs").renderFile);
 app.set("view engine", "ejs");
 
 
 //middlewares
+app.use(morgan("dev"));
 
 //routes
 app.use(require("./routes/index"));
